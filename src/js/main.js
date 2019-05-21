c4790dca6a1f74576eb744d1f805c4b75866574b
Bagautdinov Ruslan <molodoybaja@gmail.com>
2019-05-21 20:29:06 +0500

    доверстал главную страницу

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,20 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var userBlock = document.querySelector('.user-block');
+var pageHeader = document.querySelector('.page-header');
+var navToggle = document.querySelector('.page-header__toggle');
 
 navMain.classList.remove('main-nav--nojs');
+userBlock.classList.remove('user-block--nojs');
 
 navToggle.addEventListener('click', (e) => {
+  if (pageHeader.classList.contains('page-header--closed')) {
+    pageHeader.classList.remove('page-header--closed');
+    pageHeader.classList.add('page-header--opened');
+  } else {
+    pageHeader.classList.add('page-header--closed');
+    pageHeader.classList.remove('page-header--opened');
+  }
+
   if (navMain.classList.contains('main-nav--closed')) {
     navMain.classList.remove('main-nav--closed');
     navMain.classList.add('main-nav--opened');
